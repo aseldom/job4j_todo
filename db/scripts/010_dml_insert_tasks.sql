78dc2fd71c8d053f529b448894e9d7bef09c2ef7
aseldom <a.seldom@gmail.com>
2023-11-21 08:07:19 +0600

    fix - remove script dml

--- db/scripts/010_dml_insert_tasks.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO tasks (title, description, created, done) VALUES
-('task1', 'do exercises', '2023-11-16 08:00:00', 'true'),
-('task2', 'wash', '2023-11-16 09:00:00', 'true'),
-('task3', 'have breakfast', '2023-11-16 09:30:00', 'false'),
-('task4', 'read a book', '2023-11-16 10:00:00', 'false'),
-('task5', 'fly to the moon', '2023-11-16 12:00:00', 'false');
